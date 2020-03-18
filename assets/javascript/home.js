e651a9e320d9e8fd2b2567c920a42c6886bf7170
AshfoMon <ashfordmonte@gmail.com>
2020-03-18 17:28:13 -0500

    Refined home.js code

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -19,11 +19,49 @@ $('input.autocomplete').autocomplete({
   }
 });
 
-var address = "";
-$("#submit").on("click", function(event) {
+function addressValidation(address, addressCook) {
+  console.log("Address Validation Called");
+  var key = "AlAWRCVzQaselab1SC0uQ6Iv6pJ4jmNux7hGgBmewvh7UVQmlWnTuMYMIGE3jdxM";
+  var address1 = address;
+  var address2 = addressCook;
+
+  address1 = encodeURI(address1);
+  address2 = encodeURI(address2);
+
+  var queryURL = "http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=" +
+    address1 + "&wp.1=" + address2 + "&distanceUnit=mi&key=" + key;
+
+  console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log("AJAX Called");
+    var data = response.resourceSets[0].resources[0];
+    console.log(data);
+
+    if(data.travelDistance > 20){
+      validator.push(0);
+    }
+    else{
+      validator.push(1);
+    }
+  });
+}
+
+var validator = [];
+$("#submit").on("click", function (event) {
   event.preventDefault();
 
-  address = $("#autocomplete-input").val().trim();
+  var address = $("#autocomplete-input").val().trim();
   console.log(address);
   $("#autocomplete-input").val("");
+
+  var cookAddress = ["7500 W Slaughter Ln, Austin, TX 78749", "300 S Lamar Blvd, Austin, TX 78704", "2103 E 16th St, Austin, TX 78702"];
+
+  for (let i = 0; i < cookAddress.length; i++) {
+    addressValidation(address, cookAddress[i]);
+  }
+  console.log(validator);
 });
