d8c142ccc06cf63c2e0d17d48db75f37970baa09
AshfoMon <ashfordmonte@gmail.com>
2020-03-20 17:57:58 -0500

    Selector and dynamic recipes work

--- cookPages/assets/javascript/app.js
+++ cookPages/assets/javascript/app.js
@@ -19,6 +19,7 @@ dataRef.ref().on("value", function (snapshot) {
   console.log(snapshot.val().address);
   address = snapshot.val().address;
 })
+
 var itemList = [];
 var item1 = $("#item-1").text();
 var item2 = $("#item-2").text();
@@ -26,27 +27,38 @@ var item3 = $("#item-3").text();
 item1 = item1.substring(0, item1.length - 5);
 item2 = item2.substring(0, item2.length - 5);
 item3 = item3.substring(0, item3.length - 5);
+itemList.push(item1);
+itemList.push(item2);
+itemList.push(item3);
 
-
-for (let i = 0; i < 3; i++) {
-  recipeAssigner();
+for (let i = 0; i < itemList.length; i++) {
+  recipeAssigner(itemList[i], i);
 }
 
-function recipeAssigner() {
+function recipeAssigner(item, index) {
   var key = "98cdf2c2b9f13779be5b1cdf9fe60347";
   var appID = "9ad3a6a6";
 
-  var queryURL = "https://api.edamam.com/search?q=tempura&app_id=" +
+  var queryURL = "https://api.edamam.com/search?q=" + item +"&app_id=" +
     appID + "&app_key=" + key;
 
   console.log(queryURL);
 
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    dataType: "jsonp"
   }).then(function(response) {
-    console.log(response);
-    var recipe = response.hits[0].recipe;
-    console.log(recipe);
+    index++;
+    // console.log(response);
+    var recipeURL = response.hits[0].recipe.url;
+    // console.log(recipeURL);
+    $("#recipe" + index).attr("href", recipeURL);
   })
 }
+
+var selectedFood = "";
+$("select").change(function() {
+  selectedFood = $("select option:selected").text();
+  console.log(selectedFood);
+})
